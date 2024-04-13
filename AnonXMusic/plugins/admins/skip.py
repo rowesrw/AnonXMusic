d966e91e7e38873b069114e2d95af16c4dd88f63
StAr MuSiC <129057401+rowes-star@users.noreply.github.com>
2024-04-13 19:40:52 +0200

    Update skip.py
    Signed-off-by: StAr MuSiC <129057401+rowes-star@users.noreply.github.com>

--- AnonXMusic/plugins/admins/skip.py
+++ AnonXMusic/plugins/admins/skip.py
@@ -15,7 +15,7 @@ from config import BANNED_USERS
 
 @app.on_message(
     filters.command(["skip","تخطي","الي بعدو","هات الي بعدو" "cskip", "next", "cnext"], "")  & ~BANNED_USERS
-)ض
+)
 @AdminRightsCheck
 async def skip(cli, message: Message, _, chat_id):
     if not len(message.command) < 2:

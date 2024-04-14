033c96eb0c6abe6d5186505f8cb609ab03c0993e
StAr MuSiC <129057401+rowes-star@users.noreply.github.com>
2024-04-14 18:40:57 +0200

    Create song.py
    Signed-off-by: StAr MuSiC <129057401+rowes-star@users.noreply.github.com>

---
+++ AnonXMusic/plugins/tools/song.py
@@ -0,0 +1,78 @@
+import os
+import requests
+
+import aiohttp
+import aiofiles
+
+import yt_dlp
+from yt_dlp import YoutubeDL
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+from pyrogram.types import Message, InputTextMessageContent
+from youtube_search import YoutubeSearch
+
+from AnonXMusic import app
+
+def remove_if_exists(path):
+    if os.path.exists(path):
+        os.remove(path)
+
+
+@app.on_message(filters.command(["/song", "بحث", "تحميل", "يوت"], ""))
+async def song_downloader(client, message: Message):
+    query = " ".join(message.command[1:])
+    m = await message.reply_text("<b>⇜ جـارِ البحث عـن المقطـع الصـوتـي . . .</b>")
+    ydl_ops = {
+        'format': 'bestaudio[ext=m4a]',
+        'keepvideo': True,
+        'prefer_ffmpeg': False,
+        'geo_bypass': True,
+        'outtmpl': '%(title)s.%(ext)s',
+        'quite': True,
+    }
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        title = results[0]["title"][:40]
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f"{title}.jpg"
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, "wb").write(thumb.content)
+        duration = results[0]["duration"]
+
+    except Exception as e:
+        await m.edit("- لم يتم العثـور على نتائج ؟!\n- حـاول مجـدداً . . .")
+        print(str(e))
+        return
+    await m.edit("<b>⇜ جـارِ التحميل  . . .</b>")
+    try:
+        with yt_dlp.YoutubeDL(ydl_ops) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = f"• Uploader By @{app.username} ."
+        host = str(info_dict["uploader"])
+        secmul, dur, dur_arr = 1, 0, duration.split(":")
+        for i in range(len(dur_arr) - 1, -1, -1):
+            dur += int(float(dur_arr[i])) * secmul
+            secmul *= 60
+        await m.edit("<b>⇜ جـارِ الرفـع . . .</b>")
+        await message.reply_audio(
+            audio=audio_file,
+            caption=rep,
+            title=title,
+            performer=host,
+            thumb=thumb_name,
+            duration=dur,
+        )
+        await m.delete()
+
+    except Exception as e:
+        await m.edit(" error, wait for bot owner to fix")
+        print(e)
+
+    try:
+        remove_if_exists(audio_file)
+        remove_if_exists(thumb_name)
+    except Exception as e:
+        print(e)

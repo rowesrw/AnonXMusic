d35965b115afe07b5866ac331282ea3d3233b463
StAr MuSiC <129057401+rowes-star@users.noreply.github.com>
2024-04-17 13:51:47 +0200

    Update id.py
    Signed-off-by: StAr MuSiC <129057401+rowes-star@users.noreply.github.com>

--- AnonXMusic/plugins/tools/id.py
+++ AnonXMusic/plugins/tools/id.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram import Client as app
+from AnonXMusic import app
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.enums import ChatMemberStatus, ChatMemberStatus
 

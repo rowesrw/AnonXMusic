4f47c37031210c43212de1a1feee790cc1b28724
StAr MuSiC <129057401+rowes-star@users.noreply.github.com>
2024-04-18 04:41:40 +0200

    Update thumbnails.py
    Signed-off-by: StAr MuSiC <129057401+rowes-star@users.noreply.github.com>

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -21,7 +21,7 @@ def changeImageSize(maxWidth, maxHeight, image):
     return newImage
 
 
-async def gen_thumb(videoid):
+async def get_thumb(videoid):
     if os.path.isfile(f"cache/{videoid}.png"):
         return f"cache/{videoid}.png"
 

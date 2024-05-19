227222b697f0e50bce8f2dda1b7c050be9409734
StAr MuSiC <129057401+rowes-star@users.noreply.github.com>
2024-05-19 21:45:05 +0300

    Update filters.py
    Signed-off-by: StAr MuSiC <129057401+rowes-star@users.noreply.github.com>

--- strings/filters.py
+++ strings/filters.py
@@ -5,14 +5,6 @@ from pyrogram.types import Message
 from pyrogram.errors import UserNotParticipant
 from AnonXMusic import app
 
-channel = "vc_xm" # Your Channel
-async def subscription(_, __: Client, message: Message):
-    user_id = message.from_user.id
-    try: await app.get_chat_member(channel, user_id)
-    except UserNotParticipant: return False
-    return True
-    
-subscribed = filters.create(subscription)
 
 other_filters = filters.group & ~filters.via_bot & ~filters.forwarded
 other_filters2 = (

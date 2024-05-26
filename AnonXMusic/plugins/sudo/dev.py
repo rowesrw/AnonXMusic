9f0889c6408e3a37ab7d2d6f9fcf2821d33fe0bd
StAr MuSiC <129057401+rowes-star@users.noreply.github.com>
2024-05-26 15:21:49 +0300

    Create dev.py
    Signed-off-by: StAr MuSiC <129057401+rowes-star@users.noreply.github.com>

---
+++ AnonXMusic/plugins/sudo/dev.py
@@ -0,0 +1,35 @@
+import random
+from pyrogram import Client, filters, idle
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from AnonXMusic import app 
+import os
+
+@Client.on_message(filters.command(["المطور", "مطور","مطور البوت"], ""))
+async def dev(client: Client, message: Message):
+     if await joinch(message):
+            return
+     bot_username = client.me.username
+     user = await client.get_chat(chat_id=5749137933)
+     name = user.first_name
+     username = user.username 
+     bio = user.bio
+     user_id = user.id
+     photo = user.photo.big_file_id
+     photo = await client.download_media(photo)
+     link = f"https://t.me/{message.chat.username}"
+     title = message.chat.title if message.chat.title else message.chat.first_name
+     chat_title = f"User : {message.from_user.mention} \nChat Name : {title}" if message.from_user else f"Chat Name : {message.chat.title}"
+     try:
+      await client.send_message(username, f"**هناك شخص بالحاجه اليك عزيزي المطور الأساسي**\n{chat_title}\nChat Id : `{message.chat.id}`",
+      reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{title}", url=f"{link}")]]))
+     except:
+        pass
+     await message.reply_photo(
+     photo=photo,
+     caption=f"**Developer Name : {name}** \n**Devloper Username : @{username}**\n**{bio}**",
+     reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{name}", user_id=f"{user_id}")]]))
+     try:
+       os.remove(photo)
+     except:
+        pass
+       

18b204e12a61ff19a0ae736984094b5b86870b9f
StAr MuSiC <129057401+rowes-star@users.noreply.github.com>
2024-05-29 16:20:53 +0300

    Update song.py
    Signed-off-by: StAr MuSiC <129057401+rowes-star@users.noreply.github.com>

--- AnonXMusic/plugins/tools/song.py
+++ AnonXMusic/plugins/tools/song.py
@@ -21,7 +21,7 @@ def remove_if_exists(path):
 
 
 
-@app.on_message(command(["/song", "بحث", "/music"]))
+@app.on_message(command(["/song", "بحث", "/music","يوت"]))
 async def song_downloader(client, message: Message):
     query = " ".join(message.command[1:])
     m = await message.reply_text("<b>⇜ جـارِ البحث عـن المقطـع الصـوتـي . . .</b>")

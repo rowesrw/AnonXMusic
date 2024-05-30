3b7472e3d2c3c985c29866b3058abae02432ae7b
StAr MuSiC <129057401+rowes-star@users.noreply.github.com>
2024-05-30 11:42:23 +0300

    Create المطور.py
    Signed-off-by: StAr MuSiC <129057401+rowes-star@users.noreply.github.com>

---
+++ AnonXMusic/plugins/play/المطور.py
@@ -0,0 +1,45 @@
+
+import asyncio
+from strings.filters import command
+from AnonXMusic.utils.decorators import AdminActual
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+    InputMediaPhoto,
+    Message,
+)
+from AnonXMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from config import OWNER_ID
+
+@app.on_message(filters.command("نادي المطور", [".", ""]) & filters.group)
+async def call_dev(client: Client, message: Message):
+    chat = message.chat.id
+    gti = message.chat.title
+    link = await app.export_chat_invite_link(chat)
+    usr = await client.get_users(message.from_user.id)
+    chatusername = f"@{message.chat.username}"
+    user_id = message.from_user.id
+    user_ab = message.from_user.username
+    user_name = message.from_user.first_name
+    buttons = [[InlineKeyboardButton(gti, url=f"{link}")]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await app.send_message(OWNER_ID, f"- قام {message.from_user.mention}\n"
+                                     f"- بمناداتك عزيزي المطور\n"
+                                     f"- الأيدي {user_id}\n"
+                                     f"- اليوزر @{user_ab}\n"
+                                     f"- ايدي المجموعة {message.chat.id}\n"
+                                     f"- الرابط {chatusername}",
+                                     reply_markup=reply_markup)
+
+    # إنشاء زر "اونلاين"
+    online_button = InlineKeyboardButton("𝗥𝗼𝗪𝗲𝗦", url=f"http://t.me/R7_OX")
+  
+    await message.reply_photo("https://telegra.ph/file/99853ed28bfc7bb217c94.jpg" ,caption = f"~ **تم إرسال النداء إلى مطور  البوت\n\n-› 𝗥𝗼𝗪𝗲𝗦 -› @R7_OX .",
+                             disable_web_page_preview=True,
+                             reply_markup=InlineKeyboardMarkup([[online_button]]))

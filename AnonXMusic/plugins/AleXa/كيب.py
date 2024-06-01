41a2363e9252de3d21881d146b6cbb1b647822ac
RoWeS <129057401+rowes-star@users.noreply.github.com>
2024-06-01 18:22:59 +0300

    Update كيب.py
    Signed-off-by: RoWeS <129057401+rowes-star@users.noreply.github.com>

--- AnonXMusic/plugins/AleXa/كيب.py
+++ AnonXMusic/plugins/AleXa/كيب.py
@@ -67,19 +67,3 @@ async def down(client, message):
           m = await message.reply("**- بخدمتك حجي خفيت الازرار\n- اذا تريد تطلعها مرة ثانية اكتب الاوامر**", reply_markup= ReplyKeyboardRemove(selective=True))
 
 
-@app.on_message(filters.group & command("طريقة ربط القنوات"))
-async def dowhmo(client: Client, message: Message):
-    await message.reply_text("""- هلا والله\n◌**عشان تشغل بالقنوات لازم تسوي بعض الخطوات وهي◌** :\n\n1 -› تدخل البوت قناتك وترفعه مشرف\n2 -› ترجع للقروب وتكتب { **ربط + يوزر القناة** }\n3 -› **اضغط على زر اوامر التشغيل عشان تعرف كيف تشغل**..""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "قناة السورس", url=f"https://t.me/Source_Ze"),
-                ],[
-                    InlineKeyboardButton(
-                        "• ضيفني لقروبك 🎻", url=f"https://t.me/{app.username}?startgroup=true"),
-                ],
-            ]
-        ),
-        disable_web_page_preview=True
-    )

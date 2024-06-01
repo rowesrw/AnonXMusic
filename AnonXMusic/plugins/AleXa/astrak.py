494bf93309d231be7f39ccf9341d1ab011277da0
RoWeS <129057401+rowes-star@users.noreply.github.com>
2024-06-01 12:27:39 +0300

    Update astrak.py
    Signed-off-by: RoWeS <129057401+rowes-star@users.noreply.github.com>

--- AnonXMusic/plugins/AleXa/astrak.py
+++ AnonXMusic/plugins/AleXa/astrak.py
@@ -16,11 +16,11 @@ subscribed = filters.create(subscription)
 
 @app.on_message(~subscribed)
 async def checker(_: Client, message: Message):
-    if message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]: await message.delete()
+    if message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
     user_id = message.from_user.id
     user = message.from_user.first_name
     markup = Markup([
-        [Button("🔱 𝐒𝐎𝐔𝐑𝐂𝐄 𝐙𝐄 🔱", url=f"https://t.me/{channel}")]
+        [Button(" اشترك هنا ", url=f"https://t.me/{channel}")]
     ])
     await message.reply(
         f"عذرًا عزيزي {user}عليك الإشتراك بقناة السور أولا.",

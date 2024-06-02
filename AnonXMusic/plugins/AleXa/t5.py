abdc5b2707a28d8703d979612d38569f5104af7c
RoWeS <129057401+rowes-star@users.noreply.github.com>
2024-06-02 09:47:56 +0300

    Update t5.py
    Signed-off-by: RoWeS <129057401+rowes-star@users.noreply.github.com>

--- AnonXMusic/plugins/AleXa/t5.py
+++ AnonXMusic/plugins/AleXa/t5.py
@@ -1,3 +1,16 @@
+import asyncio
+import os
+import time
+import requests
+from pyrogram import filters
+import random
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from strings.filters import command
+from AnonXMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AnonXMusic import app
+from random import  choice, randint
+
 @app.on_message(filters.command("تخ", [".", ""]) & filters.group)
 async def ihd(client: Client, message: Message):
     url = f"https://telegra.ph/file/2b3f97cdbf166149b79b6.mp4"

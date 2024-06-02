cc367debd52355f23c415de66ef3d71d0c5edc64
RoWeS <129057401+rowes-star@users.noreply.github.com>
2024-06-02 09:57:12 +0300

    Update t5.py
    Signed-off-by: RoWeS <129057401+rowes-star@users.noreply.github.com>

--- AnonXMusic/plugins/AleXa/t5.py
+++ AnonXMusic/plugins/AleXa/t5.py
@@ -16,7 +16,7 @@ async def ihd(client: Client, message: Message):
     url = f"https://telegra.ph/file/2b3f97cdbf166149b79b6.mp4" 
     msg = message.from_user.mention 
     msgg = message.reply_to_message.from_user.mention
-    await client.send_photo(message.chat.id,url,caption="≭︰قتل ↫ ⦗ {msg} ⦘\n≭︰الضحيه دا 😢 ↫ ⦗ {msgg} ⦘\nانا لله وانـا اليـه راجعـون 😢😢",
+    await client.send_animation(message.chat.id,url,caption="≭︰قتل ↫ ⦗ {msg} ⦘\n≭︰الضحيه دا 😢 ↫ ⦗ {msgg} ⦘\nانا لله وانـا اليـه راجعـون 😢😢",
     reply_markup=InlineKeyboardMarkup(
             [
                 [

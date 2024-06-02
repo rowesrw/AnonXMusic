6616d3c7ba0289e6e32102bc032979fc230d4ffa
RoWeS <129057401+rowes-star@users.noreply.github.com>
2024-06-02 09:51:33 +0300

    Update t5.py
    Signed-off-by: RoWeS <129057401+rowes-star@users.noreply.github.com>

--- AnonXMusic/plugins/AleXa/t5.py
+++ AnonXMusic/plugins/AleXa/t5.py
@@ -14,7 +14,7 @@ from random import  choice, randint
 @app.on_message(filters.command("تخ", [".", ""]) & filters.group)
 async def ihd(client: Client, message: Message):
     url = f"https://telegra.ph/file/2b3f97cdbf166149b79b6.mp4"
-    await client.send_photo(message.chat.id,url,caption="≭︰قتل ↫ ⦗ {message.from_user.mention} ⦘\n≭︰الضحيه دا 😢 ↫ ⦗ {message.reply_to_message.from_user.mention} ⦘\nانا لله وانـا اليـه راجعـون 😢😢",parse_mode="html",
+    await client.send_photo(message.chat.id,url,caption="≭︰قتل ↫ ⦗ {message.from_user.mention} ⦘\n≭︰الضحيه دا 😢 ↫ ⦗ {message.reply_to_message.from_user.mention} ⦘\nانا لله وانـا اليـه راجعـون 😢😢",
     reply_markup=InlineKeyboardMarkup(
             [
                 [

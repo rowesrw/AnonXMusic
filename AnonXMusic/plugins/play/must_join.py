31b3c8f0892f188fcf511131df692ead70bd9b57
RoWeS <129057401+rowes-star@users.noreply.github.com>
2024-06-04 12:31:54 +0300

    Update must_join.py
    Signed-off-by: RoWeS <129057401+rowes-star@users.noreply.github.com>

--- AnonXMusic/plugins/play/must_join.py
+++ AnonXMusic/plugins/play/must_join.py
@@ -29,31 +29,3 @@ async def must_join_channel(bot: Client, msg: Message):
                 pass
     except ChatAdminRequired:
         print(f"Promote me as an admin in the MUST_JOIN chat : {MUST_JOIN} !")
-
-
-MUST_JOIN = "vc_xm"
-
-@app.on_message(filters.incoming & filters.group, group=-1)
-async def must_join_channel(bot: Client, msg: Message):
-    if not MUST_JOIN:  # Not compulsory
-        return
-    try:
-        try:
-            await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
-        except UserNotParticipant:
-            if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
-            else:
-                chat_info = await bot.get_chat(MUST_JOIN)
-                link = chat_info.invite_link
-            try:
-                await msg.reply_photo(photo="https://telegra.ph/file/6d2c75e56f63a0668ec6e.jpg", caption=f"↯︙ عـݪـيك اެݪاެشـتراެك فـي قـنـاެة اެݪـبـوت 📻 .\n↯︙يوزر القناة ( {link} ) .",
-                    reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("اضغط هنا للاشتراك💤", url=f"{link}")]
-                    ])
-                )
-                await msg.stop_propagation()
-            except ChatWriteForbidden:
-                pass
-    except ChatAdminRequired:
-        print(f"Promote me as an admin in the MUST_JOIN chat : {MUST_JOIN} !")

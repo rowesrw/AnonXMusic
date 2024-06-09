437af08f661b80c7819ecf35a315d8c3b3b024fd
RoWeS <129057401+rowes-star@users.noreply.github.com>
2024-06-09 11:54:09 +0300

    Update stop.py
    Signed-off-by: RoWeS <129057401+rowes-star@users.noreply.github.com>

--- AnonXMusic/plugins/admins/stop.py
+++ AnonXMusic/plugins/admins/stop.py
@@ -1,16 +1,17 @@
+from MatrixMusic.plugins.play.filters import command
 from pyrogram import filters
 from pyrogram.types import Message
 
 from AnonXMusic import app
-from AnonXMusic.core.call import Anony
+from AnonXMusic.core.call import Anony 
 from AnonXMusic.utils.database import set_loop
 from AnonXMusic.utils.decorators import AdminRightsCheck
 from AnonXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
-
+#comand
 @app.on_message(
-    filters.command(["/end", "/stop", "/cend", "/cstop","ايقاف","وقف"],"") & ~BANNED_USERS
+    command(["/stop", "اسكت", "انهاء", "ايقاف"]) & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
@@ -18,6 +19,7 @@ async def stop_music(cli, message: Message, _, chat_id):
         return
     await Anony.stop_stream(chat_id)
     await set_loop(chat_id, 0)
+    user_mention = message.from_user.mention if message.from_user else "المشـرف"
     await message.reply_text(
-        _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
+        _["admin_5"].format(user_mention), reply_markup=close_markup(_)
     )
